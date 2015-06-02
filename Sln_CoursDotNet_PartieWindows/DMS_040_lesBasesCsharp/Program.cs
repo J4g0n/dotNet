91dd449f5f8d2ed7e12a8364276e6e73531e5a0b
J4g0n <simon.andreux@gmail.com>
2015-06-02 16:43:28 +0200

    add new project and exercices

--- Sln_CoursDotNet_PartieWindows/DMS_040_lesBasesCsharp/Program.cs
+++ Sln_CoursDotNet_PartieWindows/DMS_040_lesBasesCsharp/Program.cs
@@ -8,8 +8,53 @@ namespace DMS_040_lesBasesCsharp
 {
     class Program
     {
+        enum situationFamiliale
+        {
+            celibataire,
+            marie,
+            mariee,
+            divorce,
+            divorcee,
+            pacse,
+            pacsee,
+            veuf, 
+            veuve
+        }
+
+        static void displayCountries(List<string> listePays)
+        {
+            foreach (string str in listePays)
+            {
+                colorierPays(str);
+            }
+            Console.WriteLine("\n");
+        }
+
+        static void colorierPays(string pays)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(pays);
+            Console.ResetColor();
+        }
+
         static void Main(string[] args)
         {
+            int [] notes = new int[] {1, 9, 15, 7};
+            Console.WriteLine("une note : {0}", notes[3]);
+
+            string[] fetes = new string[] { "1", "fiojf" };
+
+            List<string> listePays = new List<string>();
+            listePays.Add("France");
+            listePays.Add("Allemagne");
+            listePays.Add("Italie");
+            displayCountries(listePays);
+
+            Console.WriteLine("Something");
+            listePays.RemoveAt(1);
+            displayCountries(listePays);
+            
+            Console.ReadKey();
         }
     }
 }
