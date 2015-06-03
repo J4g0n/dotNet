d69c0009f0213b0ba875dd902f8c6d7c1ab24f02
J4g0n <simon.andreux@gmail.com>
2015-06-03 12:01:04 +0200

    add POO course project

---
+++ Sln_CoursDotNet_PartieWindows/DMS_050_POO_Cours/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS_050_POO_Cours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
