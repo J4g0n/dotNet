9fba4d10a1b031f6e89934eb4f3e7f2de173b7d4
J4g0n <simon.andreux@gmail.com>
2015-06-05 14:55:40 +0200

    add forgotten file

---
+++ Sln_CoursDotNet_PartieWindows/DMS_050_POO_Exercices/ConversionsSystemeAngloSaxon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS_050_POO_Exercices
+{
+    class ConversionsSystemeAngloSaxon
+    {
+        public static double fahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5/9 ;
+        }
+
+        public static double poundToEuro(double pound)
+        {
+            return pound * 1.36;
+        }
+
+        public static double inchToCentimeters(double inch)
+        {
+            return 2.54 * inch;
+        }
+
+        public static double gallonToLitre(double gallon)
+        {
+            return gallon * 3.78;
+        }
+    }
+}
